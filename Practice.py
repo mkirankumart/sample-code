505c8b5cb0fbf6ffa14ee5533f7dea3bcb6d2e36
Kiran Kumar <kirankumartm1204@gmail.com>
2023-07-18 00:24:40 -0400

    Removed n added some code

--- Practice.py
+++ Practice.py
@@ -1,20 +1,6 @@
-import math
-import os
-import sys
-
 import requests
 
-# print(sys.version)
-print(sys.executable)
 
-
-def greet(whotogreet):
-    greeting = "hello,{}".format(whotogreet)
-    return greeting
-
-
-r = requests.get("https://youtube.com")
-print(r.status_code)
-
-na  me = input("your name?")
+me = input("your name?")
 print("hello,", name)
+print(r.ok)
